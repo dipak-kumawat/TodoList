199bf2de6f83bbb11f6bead40866cd046d5b5ee8
dipak-kumawat <dipaksk11@gmail.com>
2024-08-25 14:07:06 +0530

    mongodb uri change

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(cors());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:5173',
   methods: 'GET,POST,PUT,DELETE',
   credentials: true, // if you need to send cookies or authentication headers
 }));
 
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://dipaksk11:dipak@123@cluster0.wee8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://dipaksk11:dipak@cluster0.wee8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 app.get('/', (req, res) =>{
   res.json("Welcome to hello word ! ")
 })
